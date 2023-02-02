86c2c64fec4d808aea0b8075237ed3c3db2f91d9
rahulx81x <rahulgouri072@gmail.com>
2023-02-02 19:38:47 +0530

    Added  GetBalance, GetAllTransaction, GetCreditTransaction, GetDebitTransaction.

--- HisabKitabDAL/HKRepository.cs
+++ HisabKitabDAL/HKRepository.cs
@@ -11,5 +11,67 @@ namespace HisabKitabDAL
             context = new HisabKitabDbContext(); 
         }
 
+        public string GetBalance(int userId)
+        {
+            decimal balance = 0;
+            try
+            {
+                    var tranList = (from tran in context.Transactions select tran).ToList();
+                foreach (var tran in tranList)
+                {
+                    if (tran.Type == "C")
+                    {
+                        balance += tran.Amount;
+                    }
+                    else if (tran.Type == "D")
+                    {
+                        balance -= tran.Amount;
+                    }
+                }
+                return balance.ToString();
+            }
+            catch
+            {
+                string errMessage = "Error";
+                return errMessage  ;
+            }
+           
+        }
+        public List<Transaction>? GetAllTransaction(int UserId)
+        {
+            try
+            {
+                var tranList = (from tran in context.Transactions select tran).ToList();
+                return tranList;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public List<Transaction>? GetCreditTransaction(int UserId)
+        {
+            try
+            {
+                var tranList = (from tran in context.Transactions where tran.Type=="C" select tran).ToList();
+                return tranList;
+            }
+            catch
+            {
+                return null;
+            }
+        }
+        public List<Transaction>? GetDebitTransaction(int UserId)
+        {
+            try
+            {
+                var tranList = (from tran in context.Transactions where tran.Type == "D" select tran).ToList();
+                return tranList;
+            }
+            catch
+            {
+                return null;
+            }
+        }
     }
 }
