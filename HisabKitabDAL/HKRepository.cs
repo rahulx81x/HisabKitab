9265c3713186e8bec01be82f84e7c40a7a20a622
rahulx81x <rahulgouri072@gmail.com>
2023-02-03 09:37:53 +0530

    added AddUser, AddTran, AddCredit, AddDebit, CheckUserPassword.

--- HisabKitabDAL/HKRepository.cs
+++ HisabKitabDAL/HKRepository.cs
@@ -1,5 +1,5 @@
 ﻿using HisabKitabDAL.Models;
-
+using System.Diagnostics.SymbolStore;
 
 namespace HisabKitabDAL
 {
@@ -73,5 +73,105 @@ namespace HisabKitabDAL
                 return null;
             }
         }
+
+        public bool AddUser(string username, string password)
+        {
+            User user = new User();
+            user.UserName= username;
+            user.UserPassword= password;
+            try
+            {
+                context.Users.Add(user);
+                context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+
+        #region Separate functions for adding credit and debit logs
+        //public bool AddCredit(int userId,DateTime date,decimal amount)
+        //{
+        //    Transaction tran = new Transaction();
+        //    tran.UserId= userId;
+        //    tran.Date= date;
+        //    tran.Amount= amount;
+        //    tran.Type = "C";
+        //    try
+        //    {
+        //        context.Transactions.Add(tran);
+        //        context.SaveChanges();
+        //        return true;
+        //    }
+        //    catch
+        //    {
+        //        return false;
+        //    }
+
+        //}
+        //public bool AddDebit(int userId, DateTime date, decimal amount)
+        //{
+        //    Transaction tran = new Transaction();
+        //    tran.UserId = userId;
+        //    tran.Date = date;
+        //    tran.Amount = amount;
+        //    tran.Type = "D";
+        //    try
+        //    {
+        //        context.Transactions.Add(tran);
+        //        context.SaveChanges();
+        //        return true;
+        //    }
+        //    catch
+        //    {
+        //        return false;
+        //    } 
+        #endregion
+
+        public bool AddTran(int userId, DateTime date, decimal amount, string type)
+        {
+            Transaction tran = new Transaction();
+            tran.UserId = userId;
+            tran.Date = date;
+            tran.Amount = amount;
+            tran.Type = type;
+            try
+            {
+                context.Transactions.Add(tran);
+                context.SaveChanges();
+                return true;
+            }
+            catch
+            {
+                return false;
+            }
+
+        }
+
+        public bool CheckUserPassword(string username,string password)
+        {
+            try
+            {
+                var userpwd = (from user in context.Users where user.UserName == username select user.UserPassword);
+                if(userpwd != null)
+                {
+                    if (userpwd.ToString() == password)
+                    {
+                        return true;
+                    }
+                }
+                return false;
+            }
+            catch
+            {
+                return false;
+            }
+            
+        }
+
     }
+        
+    
 }
