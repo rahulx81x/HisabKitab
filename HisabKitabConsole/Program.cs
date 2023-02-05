00fda77c22a3a6afeed34029a8c6f46326c92530
rahulx81x <rahulgouri072@gmail.com>
2023-02-05 11:50:33 +0530

    --

---
+++ HisabKitabConsole/Program.cs
@@ -0,0 +1,6 @@
+﻿//See https://aka.ms/new-console-template for more information
+using HisabKitabDAL.Models;
+using HisabKitabDAL;
+//HKRepository repository = new HKRepository();
+//bool result = repository.AddUser("name1", "password9");
+Console.WriteLine("xoxo");
