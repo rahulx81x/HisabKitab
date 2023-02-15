a2a69af2ad68f9c70768e453ddb5da3b4104ac2e
rahulx81x <rahulgouri072@gmail.com>
2023-02-15 12:41:11 +0530

    added balance and add transaction functions

--- HisabKitabMVC/Controllers/UserController.cs
+++ HisabKitabMVC/Controllers/UserController.cs
@@ -1,15 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using HisabKitabDAL;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
 
 namespace HisabKitabMVC.Controllers
 {
     public class UserController : Controller
     {
-        
+        HKRepository repository;
+        public UserController()
+        {
+            repository = new HKRepository(); 
+        }
 
         public IActionResult Index()
         {
             ViewBag.user = HttpContext.Session.GetString("userName");
+            ViewBag.userId = repository.GetUserId(ViewBag.user);
+            ViewBag.balance = repository.GetBalance(ViewBag.userId);
             return View();
+        }
+        public ActionResult AddTransaction()
+        {
+            List<SelectListItem> typeOptions = new List<SelectListItem>
+            {
+                new SelectListItem { Value = "C", Text = "Credit" },
+                new SelectListItem { Value = "D", Text = "Debit" }
+            };
+            var model = new Models.Transaction();
+            model.TypeOptions = typeOptions.ToList();
+            return View(model);
+        }
+        public ActionResult SaveTransaction(Models.Transaction tran)
+        {
+            try
+            {
+                string user = HttpContext.Session.GetString("userName");
+                int userId = repository.GetUserId(user);
+                bool res = repository.AddTran(userId, tran.Date, tran.Amount, tran.Type.ToString(), tran.Remarks=" ");
+                if (res)
+                {
+                    return RedirectToAction("Index");
+                }
+                return RedirectToAction("AddTransaction");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message+" in User Controllr,AddTran");
+                return RedirectToAction("AddTransaction");
+            }
         }
     }
 }
---
+++ HisabKitabMVC/Models/Transaction.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace HisabKitabMVC.Models
+{
+    public class Transaction
+    {
+        [ScaffoldColumn(false)]
+        public int UserId { get; set; }
+
+        [ScaffoldColumn(false)]
+        public int TranId { get; set; }
+
+        public DateTime Date { get; set; }
+        [Required(ErrorMessage = "Amount is mandatory.")]
+        public decimal Amount { get; set; }
+        [Required(ErrorMessage = "Type is mandatory.")]
+        public string Type { get; set; } = null!;
+
+        [StringLength(maximumLength: 100)]
+        [DisplayName("Transaction Remarks")]
+        public string? Remarks { get; set; }
+
+        [ScaffoldColumn(false)]
+        public List<SelectListItem>? TypeOptions { get; set; }
+
+    }
+}
