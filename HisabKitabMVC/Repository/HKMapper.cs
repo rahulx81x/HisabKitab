d418b09f42bf068367472cda9dfb01813360231c
rahulx81x <rahulgouri072@gmail.com>
2023-03-20 20:12:15 +0530

    added mapper class

---
+++ HisabKitabMVC/Repository/HKMapper.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using HisabKitabDAL.Models;
+using System.Runtime;
+
+namespace HisabKitabMVC.Repository
+{
+    public class HKMapper:Profile
+    {
+        public HKMapper()
+        {
+            CreateMap<Transaction, Models.Transaction>();
+            CreateMap<Models.Transaction, Transaction>();
+        }
+    }
+}
